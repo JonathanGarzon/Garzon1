392589b65a6cb3980a7636cccc6041c2edf617b3
Jonathan Garzon <garzonart@gmail.com>
2020-02-08 13:20:13 -0600

    Primera app

---
+++ lib/Screens/settings.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:primera_app/Screens/MyHomeState.dart';
+
+class SettingsScreen extends StatefulWidget {
+  static const String routeName = "/settings";
+
+  @override
+  MyHomeState createState() => MyHomeState();
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:primera_app/Screens/account.dart';
+import 'package:primera_app/Screens/home.dart';
+import 'package:primera_app/Screens/settings.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen(), // route for home is '/' implicitly
+    routes: <String, WidgetBuilder>{
+      // define the routes
+      SettingsScreen.routeName: (BuildContext context) => SettingsScreen(),
+      AccountScreen.routeName: (BuildContext context) => AccountScreen(),
+    },
+  ));
+}
